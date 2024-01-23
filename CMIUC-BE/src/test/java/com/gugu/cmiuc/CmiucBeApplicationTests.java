689fa9d10e95a6d94665e7684ef80632c170c511
tddudwns1 <tjddudwns1@naver.com>
2024-01-23 16:23:03 +0900

    [init] BE 프로젝트 생성

---
+++ CMIUC-BE/src/test/java/com/gugu/cmiuc/CmiucBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gugu.cmiuc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmiucBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
