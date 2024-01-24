7beabc12d99cedc7feab4e99a56cbd509a6c4d17
배성규 <sgbae777@gmail.com>
2024-01-25 00:04:33 +0900

    🍱 탭 로고 추가 및 수정, 기본 폴더 구조 생성 및 로그인 컴포넌트 추가

---
+++ CMIUC-FE/src/components/Loading.jsx
@@ -0,0 +1,12 @@
+import Spinner from "../assets/loading.gif";
+const Loading = () => {
+    return(
+        <div>
+            <h3>로딩중...</h3>
+            <img src={Spinner} alt="Loading" width="10%" />
+        </div>
+    )
+};
+
+
+export default Loading;
