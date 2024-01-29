2eac4d5d8ddb63e4bacb1ef0bc4f8c694a2722f9
Soomin Kim <min120926@gmail.com>
2024-01-29 10:57:38 +0900

    🔥 불필요한 코드 삭제

--- CMIUC-BE/src/main/java/com/gugu/cmiuc/domain/chat/dto/ChatRoomDTO.java
+++ CMIUC-BE/src/main/java/com/gugu/cmiuc/domain/chat/dto/ChatRoomDTO.java
@@ -13,10 +13,6 @@ public class ChatRoomDTO implements Serializable {
 
     private static final long serialVersionUID = 6494678977089006639L;
 
-    public ChatRoomDTO() {
-
-    }
-
     public enum RoomType {
         FRIEND, GAME
     }
