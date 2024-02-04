bdf16864cbf537445cd18e33cc6401fe66b44267
Soomin Kim <min120926@gmail.com>
2024-02-04 17:30:12 +0900

    🐛 웹 소켓 데이터 타입 수정

--- CMIUC-BE/src/main/java/com/gugu/cmiuc/global/stomp/pubsub/RedisSubscriber.java
+++ CMIUC-BE/src/main/java/com/gugu/cmiuc/global/stomp/pubsub/RedisSubscriber.java
@@ -1,6 +1,8 @@
 package com.gugu.cmiuc.global.stomp.pubsub;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.gugu.cmiuc.global.result.error.ErrorCode;
+import com.gugu.cmiuc.global.result.error.exception.CustomException;
 import com.gugu.cmiuc.global.stomp.dto.DataDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -24,15 +26,36 @@ public class RedisSubscriber {
             DataDTO data = objectMapper.readValue(publishMessage, DataDTO.class);
             log.info("[서버] 메세지를 DTO에 맵핑 : {}", data.getType());
 
-            if (DataDTO.DataType.FRIEND_CHAT.equals(data.getType()) || DataDTO.DataType.ENTER.equals(data.getType())) { // 친구 채팅
-                log.info("친구 채팅방 구독자에게 메시지 발송");
+            // 친구 채팅 메세지 발송
+            if (DataDTO.DataType.FRIEND_CHAT.equals(data.getType())) { // 친구 채팅
+                log.info(" [친구 채팅] 구독자에게 메시지 드디어 발송");
 
                 // 채팅방을 구독한 클라이언트에게 메시지 발송
                 messagingTemplate.convertAndSend("/sub/friends/chat/" + data.getRoomId(), data);
-            } else {
-                log.info("[서버] 메세지를 DTO에 맵핑 : {}", data.getRoomId());
-                // 채팅방을 구독한 클라이언트에게 메시지 발송
-                messagingTemplate.convertAndSend("/sub/chat/room/" + data.getRoomId(), data);
+
+            // 게임 시작 전 메시지 발송
+            } else if (DataDTO.DataType.ENTER.equals(data.getType()) ||
+                    DataDTO.DataType.START.equals(data.getType()) ||
+                    DataDTO.DataType.EXIT.equals(data.getType())) {
+
+                log.info(" [게임 room] 구독자에게 게임 시작 전 메세지 전송 : {}", data.getData());
+                // 게임룸을 구독한 클라이언트에게 채팅 메시지 발송
+                messagingTemplate.convertAndSend("/sub/games/wait" + data.getRoomId(), data);
+
+            // 게임 시작 후 메세지 발송
+            } else if (DataDTO.DataType.NEW_ROUND_SET.equals(data.getType()) ||
+                    DataDTO.DataType.GAME_END_CAT_WIN.equals(data.getType()) ||
+                    DataDTO.DataType.GAME_END_MOUSE_WIN.equals(data.getType())) {
+
+                log.info(" [게임 진행] 구독자에게 data 전송 : {}", data.getData());
+                messagingTemplate.convertAndSend("/sub/games/play" + data.getRoomId(), data);
+
+            // 게임 채팅 메세지 발송
+            } else if (DataDTO.DataType.GAME_CHAT.equals(data.getType())) {
+
+                log.info(" [게임 채팅] 구독자에게 채팅 메시지 전송 ", data.getData());
+                messagingTemplate.convertAndSend("/sub/games/chat" + data.getRoomId(), data);
+
             }
 
         } catch (Exception e) {
