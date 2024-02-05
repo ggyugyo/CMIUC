86d165a1aa0642d22506b2ed3dcc2b8237fc639b
배성규 <sgbae777@gmail.com>
2024-02-05 23:48:04 +0900

    🚑 프론트 배포 설정

--- CMIUC-FE/vite.config.js
+++ CMIUC-FE/vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+const react = require("@vitejs/plugin-react");
+const path = require("path");
 
 // https://vitejs.dev/config/
-export default defineConfig({
+module.exports = defineConfig({
+  base: "./",
+  server: {
+    port: 5173,
+    host: "0.0.0.0",
+  },
   plugins: [react()],
-})
+  resolve: {
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+  },
+  assetsInclude: ["**/*.jpg", "**/*.png", "**/*.svg", "**/*.ttf"],
+});
