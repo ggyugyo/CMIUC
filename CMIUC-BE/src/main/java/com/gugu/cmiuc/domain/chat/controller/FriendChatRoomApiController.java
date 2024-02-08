b6ae8401796a9a73b93c718c9d089d6e7402119e
Soomin Kim <min120926@gmail.com>
2024-02-08 14:03:00 +0900

    :bug: 이전 채팅 기록 로직 수정
    마지막 메시지 아이디로 이전 채팅 기록 조회하는 로직 수정

--- CMIUC-BE/src/main/java/com/gugu/cmiuc/domain/chat/controller/FriendChatRoomApiController.java
+++ CMIUC-BE/src/main/java/com/gugu/cmiuc/domain/chat/controller/FriendChatRoomApiController.java
@@ -2,7 +2,6 @@ package com.gugu.cmiuc.domain.chat.controller;
 
 import com.gugu.cmiuc.domain.chat.dto.FriendChatMessageDTO;
 import com.gugu.cmiuc.domain.chat.dto.FriendChatRoomDTO;
-import com.gugu.cmiuc.domain.chat.repository.ChatRoomRepository;
 import com.gugu.cmiuc.domain.chat.service.FriendChatRoomService;
 import com.gugu.cmiuc.global.stomp.repository.StompRepository;
 import lombok.RequiredArgsConstructor;
@@ -22,7 +21,6 @@ import java.util.List;
 public class FriendChatRoomApiController {
 
     private final StompRepository stompRepository;
-    private final ChatRoomRepository roomRepository;
     private final FriendChatRoomService chatRoomService;
 
     // 채팅방 조회
@@ -32,26 +30,26 @@ public class FriendChatRoomApiController {
         return ResponseEntity.ok(chatRooms);
     }
 
-    // 채팅방 아이디로 채팅 메세지 100개씩 가져오기
-    @GetMapping("/room/{roomId}/messages")
-    public ResponseEntity<?> getPreviousMessages(@PathVariable(value = "roomId") String roomId, Pageable pageable) {
-        Page<FriendChatMessageDTO> messages = chatRoomService.getLastChatMessage(roomId, pageable);
-        return ResponseEntity.ok(messages);
-    }
-
-    // 채팅방 아이디로 해당 채팅방 전체 메세지 수 가져오기
-    @GetMapping("/room/{roomId}/count")
-    public ResponseEntity<Integer> getMessageCount(@PathVariable(value = "roomId") String roomId) {
-        int count = chatRoomService.getMessageCount(roomId);
-        return ResponseEntity.ok(count);
-    }
+    //// 채팅방 아이디로 채팅 메세지 100개씩 가져오기
+    //@GetMapping("/room/{roomId}/messages")
+    //public ResponseEntity<?> getPreviousMessages(@PathVariable(value = "roomId") String roomId, Pageable pageable) {
+    //    Page<FriendChatMessageDTO> messages = chatRoomService.getLastChatMessage(roomId, pageable);
+    //    return ResponseEntity.ok(messages);
+    //}
 
     // 마지막 메세지 id로 다음 채팅 기록 가져오기
     @GetMapping("/room/{roomId}/messages/{lastIdx}")
-    public ResponseEntity<?> getNextMessages(@PathVariable("roomId") String roomId, @PathVariable("lastIdx") Long lastIdx, Pageable pageable) {
+    public ResponseEntity<?> getPreviousMessages(@PathVariable("roomId") String roomId, @PathVariable("lastIdx") Long lastIdx, Pageable pageable) {
 
         Page<FriendChatMessageDTO> messages = chatRoomService.getPreviousChatMessage(roomId, lastIdx, pageable);
         return ResponseEntity.ok(messages);
 
+    }
+
+    // 채팅방 아이디로 해당 채팅방 전체 메세지 수 가져오기
+    @GetMapping("/room/{roomId}/count")
+    public ResponseEntity<?> getMessageCount(@PathVariable(value = "roomId") String roomId) {
+        Long count = chatRoomService.getMessageCount(roomId);
+        return ResponseEntity.ok(count);
     }
 }
--- CMIUC-BE/src/main/java/com/gugu/cmiuc/domain/chat/dto/FriendChatMessageDTO.java
+++ CMIUC-BE/src/main/java/com/gugu/cmiuc/domain/chat/dto/FriendChatMessageDTO.java
@@ -11,12 +11,14 @@ public class FriendChatMessageDTO {
 
     private Long memberId;
     private String sender; // 메시지 보낸사람
+    private Long messageId;
     private String message; // 메시지
 
     @Builder
-    public FriendChatMessageDTO(Long memberId, String sender, String message) {
+    public FriendChatMessageDTO(Long memberId, String sender, Long messageId, String message) {
         this.memberId = memberId;
         this.sender = sender;
+        this.messageId = messageId;
         this.message = message;
     }
 }
