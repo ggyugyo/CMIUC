89759405c2fbef77373ea803a37e0c78e3fb72bd
김태용 <94teo@naver.com>
2024-02-13 21:30:16 +0900

    🚑 Fix self-mic setting when user has card 1 in DRAW_CARD state

--- CMIUC-FE/src/components/game/GameVideoListItemSetting.jsx
+++ CMIUC-FE/src/components/game/GameVideoListItemSetting.jsx
@@ -3,22 +3,24 @@ import { GameContext } from "./GameLogic";
 import { ViduContext } from "../../pages/Game";
 
 export const GameVideoListItemSetting = ({ streamManager, selfVideo }) => {
-  const { gameData } = useContext(GameContext);
+  const { gameData, gameState } = useContext(GameContext);
   const { mainStreamManager, setSelfMic } = useContext(ViduContext);
 
   const videoRef = useRef();
 
   const muteUser = [...gameData.gameUsers].find((user) => {
-    if (user.cards.includes(1)) {
+    if (gameState === "DRAW_CARD" && user.cards.includes(1)) {
       return user;
-    }
+    } else return undefined;
   });
 
   if (mainStreamManager?.stream?.connection?.data !== undefined) {
     let selfData = JSON.parse(mainStreamManager.stream.connection.data);
     let selfName = selfData.clientData;
-    if (muteUser.nickname === selfName) {
+    if (muteUser?.nickname === selfName) {
       setSelfMic(false);
+    } else {
+      setSelfMic(true);
     }
   }
 
