55572110616eb7f7ad1a931b30b67bf695dc0fb2
tddudwns1 <tjddudwns1@naver.com>
2024-02-14 23:30:29 +0900

    🔧 springdoc security 기능 추가

--- CMIUC-BE/src/main/java/com/gugu/cmiuc/global/config/SwaggerConfig.java
+++ CMIUC-BE/src/main/java/com/gugu/cmiuc/global/config/SwaggerConfig.java
@@ -8,6 +8,7 @@ import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 
 @Configuration
 public class SwaggerConfig {
@@ -18,9 +19,16 @@ public class SwaggerConfig {
     @Value("${springdoc.swagger-ui.info.version}")
     private String version;
 
+    final static String AUTHORIZATION = "Authorization";
+
     @Bean
     public OpenAPI openAPI() {
-        return new OpenAPI().components(new Components())
+        SecurityRequirement addSecurityItem = new SecurityRequirement();
+        addSecurityItem.addList(AUTHORIZATION);
+
+        return new OpenAPI()
+                .addSecurityItem(addSecurityItem)
+                .components(components())
                 .info(apiInfo());
     }
 
@@ -29,5 +37,19 @@ public class SwaggerConfig {
                 .title(title)
                 .description(description)
                 .version(version);
+    }
+
+    private Components components() {
+        return new Components()
+                .addSecuritySchemes(AUTHORIZATION, securityScheme());
+    }
+
+    private SecurityScheme securityScheme() {
+        return new SecurityScheme()
+                .name(HttpHeaders.AUTHORIZATION)
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat(AUTHORIZATION)
+                .in(SecurityScheme.In.HEADER);
     }
 }
